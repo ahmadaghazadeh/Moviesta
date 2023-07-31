20781da514ae48109dbe0c74d1af2e9ff9965376
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-07-31 18:03:01 +0330

    Added card expanding

--- app/src/main/java/com/ahmad/aghazadeh/moviesta/widgets/MovieRow.kt
+++ app/src/main/java/com/ahmad/aghazadeh/moviesta/widgets/MovieRow.kt
@@ -1,5 +1,6 @@
 package com.ahmad.aghazadeh.moviesta.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,7 +14,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,13 +25,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.ahmad.aghazadeh.moviesta.R
@@ -37,10 +49,10 @@ import com.ahmad.aghazadeh.moviesta.data.getMovies
 @Preview
 @Composable
 public fun MovieRow(movie: MovieItem= getMovies()[0], onItemClick: (MovieItem) -> Unit = {}) {
+    var expanded by remember { mutableStateOf(false) }
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(4.dp)
-        .height(150.dp)
         .clickable {
             onItemClick(movie)
         },
@@ -48,19 +60,43 @@ public fun MovieRow(movie: MovieItem= getMovies()[0], onItemClick: (MovieItem) -
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp
         )) {
-        Row(verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.Start)  {
+        Row(verticalAlignment = Alignment.Top,horizontalArrangement = Arrangement.Start)  {
             AsyncImage(
                 model = movie.poster,
                 contentDescription = stringResource(R.string.movie_image_description),
-                modifier = Modifier.clip(CircleShape).size(100.dp),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(100.dp),
                 )
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(text = "${movie.title}", style = MaterialTheme.typography.headlineLarge)
                 Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelLarge)
                 Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.labelLarge)
+                Icon(imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = stringResource(R.string.movie_more_infromation),
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = MaterialTheme.colorScheme.inversePrimary)
+                AnimatedVisibility(visible = expanded,modifier = Modifier.height(100.dp)) {
+                    Text(buildAnnotatedString {
+                        withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary
+                            ,fontSize =13.sp)) {
+                            append("Plot: ")
+                        }
+                        withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.secondary
+                        ,fontSize =13.sp)) {
+                            append(movie.plot)
+                        }
+                    } , style = MaterialTheme.typography.bodyLarge)
+                }
             }
 
         }
 
     }
+
 }
